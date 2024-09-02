9c4fb637ef755d0016cf5fa35af41b54d3a0be37
andebraa <andebraa@gmail.com>
2024-09-02 21:55:02 +0200

    init coomit

---
+++ getter.py
@@ -0,0 +1,153 @@
+import sys
+import base64
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+from werkzeug.exceptions import BadRequest
+from fastapi import FastAPI, HTTPException
+from typing import Any, Dict, Tuple
+from pydantic import BaseModel
+from ultralytics import YOLO
+
+sys.path.insert(1, '/home/POLITIET/abr063/git/drone_sar/')
+from tiller_light import tiller_light
+
+model_object_detection = YOLO('/home/POLITIET/abr063/git/drone_sar/runs/detect/run3/train4/weights/best.pt')
+
+app = FastAPI()
+executor = ThreadPoolExecutor(max_workers=1)
+def isBase64(sb):
+        try:
+                if isinstance(sb, str):
+                        # If there's any unicode here, an exception will be thrown and the function will return false
+                        sb_bytes = bytes(sb, 'ascii')
+                elif isinstance(sb, bytes):
+                        sb_bytes = sb
+                else:
+                        raise ValueError("Argument must be string or bytes")
+                return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+        except Exception:
+                return False
+
+
+
+def nms_tf(bboxes, pscores, threshold):
+    '''
+    https://github.com/satheeshkatipomu/nms-python/blob/master/NMS%20using%20Python%2C%20Tensorflow.ipynb
+    NMS: NMS using in-built tf.image.non_max_suppression(bboxes,scores,top_n_proposal_after_nms,iou_threshould)
+
+    Input:
+        bboxes(tensor of bounding proposals) : Bounding Box Proposals in the format (x_min,y_min,x_max,y_max)
+        threshold(float): Overlapping threshold above which proposals will be discarded.
+
+    Output:
+        filtered_bboxes(numpy array) :selected bboxes for which IOU is less than threshold.
+    '''
+    tf_format_boxes = []#bboxes are format (xmin, ymax, width, height)
+    for box in bboxes:#tf wants boxes on format (y_min, x_min, y_max, x_max)
+        tf_format_boxes.append(np.array((box[1]+box[3], box[0], box[1], box[0]+box[2])))
+    bboxes = tf.convert_to_tensor(tf_format_boxes)
+    bbox_indices = tf.image.non_max_suppression(tf_format_boxes, pscores, 100, iou_threshold = threshold)
+    filtered_bboxes = tf.gather(bboxes,bbox_indices)#Henter ut kun de bboxes som forekommer i bbox_indices
+    scores = tf.gather(pscores,bbox_indices)
+    return filtered_bboxes.numpy(), scores.numpy()
+
+
+
+def image_handler(image_bytes: bytes, pred_conf_limit: float, tiles: Tuple[int, int], overlap_percentage: float, frame_number: int) -> Dict[str, Any]:
+    """
+    base64_string = request.json['image']
+
+
+    if isBase64(base64_string):
+        pass
+    else:
+        print('non base64 string recieved')
+        raise BadRequest
+    #base64_string.encode('utf-8')
+    img_data = base64.b64decode(base64_string)
+    """    
+    image_np = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #imdecode returns BGR fomat, yolo needs rgb
+
+    frames = tiller_light(img)
+    detections = []
+    scores = []
+    for i, elem in enumerate(frames):
+        frame = elem[0]
+        x1,y2 = elem[1]
+
+        res = model_object_detection(frame)
+        #boxes_normalized = res[0].boxes.numpy().xywhn.tolist()
+        boxes_coords = res[0].boxes.numpy().xywh.tolist()
+        confidence = res[0].boxes.conf.tolist()
+
+        for e, conf in zip(boxes_coords, confidence):
+            detections.append((int(e[0]-(e[2]/2)+x1), int(e[1]-(e[3]/2)+y2), int(e[2]), int(e[3])))
+            scores.append(conf)
+
+    #passing the whole image
+    res = model_object_detection(img)
+    boxes_coords = res[0].boxes.numpy().xywh.tolist()
+    confidence = res[0].boxes.conf.tolist()
+    for e, conf in zip(boxes_coords, confidence):
+        detections.append((int(e[0]-(e[2]/2)), int(e[1]-(e[3]/2)), int(e[2]), int(e[3])))
+        scores.append(conf)
+
+            
+    #NMS
+    print(len(detections))
+    unique_detections, unique_scores = nms_tf(detections, scores, 0.2)
+
+
+    final_box, final_conf = [], []
+    for box, conf in zip(unique_detections, unique_scores):
+        if conf > pred_conf_limit:
+            final_box.append((int(box[1]), int(box[0]), int(box[3]-box[1]), int(box[0]-box[2])))
+            final_conf.append(float(conf))
+
+    print('returning!')
+    return {
+        'status': 'success',
+        'message': "Image processed sucessfully",
+        'detections': final_box, #if on gpu, add .cpu() after boxes
+        'confidence': final_conf,
+        'frame_number': frame_number
+        }
+
+
+
+class ImageRequest(BaseModel):
+    image_base64: str
+    pred_conf_limit: float
+    overlap_percentage: float
+    tiles: Tuple[int, int]
+    frame_number: int
+
+@app.post("/b64")
+async def process_image(request: ImageRequest):
+    try:
+        # Decode the base64 image
+        image_data = base64.b64decode(request.image_base64)
+    except base64.binascii.Error:
+        raise HTTPException(status_code=400, detail="Invalid base64 image data")
+
+    future = executor.submit(image_handler, image_data, request.pred_conf_limit, request.tiles, request.overlap_percentage, request.frame_number)
+    result = future.result()
+    # Pass the decoded image and additional parameters to the image handler
+    #result = image_handler(image_data, request.pred_conf_limit, request.tiles, request.overlap_percentage)
+
+
+    return result
+
+@app.get("/")
+async def root():
+    return {"message": "works"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", workers=4)
+
+
---
+++ poster.py
@@ -0,0 +1,132 @@
+import cv2
+import json
+import requests
+import base64
+import time
+import sys
+import io
+from shapely.geometry import Polygon
+from itertools import combinations
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def frame_to_base64(image):
+    # Convert the NumPy array to PIL Image
+    pil_image = Image.fromarray(image)
+    # Create an in-memory buffer
+    buffer = io.BytesIO()
+    # Save the PIL Image to the buffer in PNG format
+    pil_image.save(buffer, format='PNG')
+    # Seek the buffer to the beginning
+    buffer.seek(0)
+    base64_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    return base64_image
+
+
+def post_img(image, pred_conf_limit, tiles, overlap_percentage, frame_number):
+    str_img = frame_to_base64(image)
+    
+    headers = {'content-type': 'application/json', 'Accept': 'text/plain'}
+    payload = json.dumps({'image_base64': str_img, "pred_conf_limit": pred_conf_limit, "tiles": tiles, "overlap_percentage": overlap_percentage, "frame_number": frame_number})
+    response = requests.post('http://localhost:8000/b64', data=payload, headers=headers)
+    content = response.json()
+    detections = content['detections']
+    confidence = content['confidence']
+    return detections, confidence, response
+
+def post_image(video_path, pred_conf_limit, tiles, overlap_percentage):
+
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    # Initialize frame count
+    frame_count = 0
+    times = []
+
+    while True:
+        # Read a frame from the video
+        success, _frame = cap.read()
+        if not success:
+            print('issue with frame read')
+            break
+        detections = [] 
+        scores = []
+
+        #fig, ax = plt.subplots(4,3)
+        #axs = ax.flatten()
+        #for i,x in enumerate(frames):
+        #    axs[i].imshow(x)
+        #plt.show()
+        
+        init_time = time.time()
+        try:
+            detections, scores, response = post_img(_frame, pred_conf_limit, tiles, overlap_percentage, frame_count)
+            content = response.json()
+            if response.status_code == 200:
+                print(f"Frame {frame_count} sent successfully.")
+                print(f' results; {content["message"]}, \n {content["detections"]} \n {content["confidence"]}')
+
+        except requests.RequestException as e:
+            print(f"Error sending frame {frame_count}: {e}")
+        times.append(time.time() - init_time)
+
+        #passing the whole image
+        _fig, _ax = plt.subplots(1)
+        _ax.imshow(np.array(_frame))
+        for elem in detections:
+            print('for elem in detections')
+            print(elem)
+            _ax.add_patch(patches.Rectangle((elem[0], elem[1]), elem[2], -elem[3], linewidth = 1.5, edgecolor = 'r', facecolor='none'))
+
+        _fig.savefig(f'temp_videofile/non_nms_video/{frame_count}.png')
+
+        plt.close()
+
+        # Increment frame count
+        frame_count += 1
+        #if frame_count >= 10:
+        #    return True, times
+
+    # Release the video capture
+    cap.release()
+    return True, times
+
+
+if __name__ == '__main__':
+    pred_conf_limit = 0.7
+    overlap_percentage = 0.2
+    tiles = (3,2)
+    video_path = '../skookumCreek_nobox.mp4'
+    gpu_times = open('timings/gpu_times.txt').read().splitlines() 
+    gpu_times = []
+    with open('timings/gpu_times.txt', 'r') as fp:
+        for line in fp:
+            gpu_times.append(float(line))
+
+    bool_res, times = post_image(video_path, pred_conf_limit, tiles, overlap_percentage)
+
+    fig, ax = plt.subplots(2,1)
+    plt.title('detection times for GPU (3090) and CPU, 3x2 tiles')
+    ax[0].plot(np.arange(0, len(gpu_times)), gpu_times, label='gpu timing')
+    ax[1].plot(np.arange(0, len(times)), times, label='cpu timing')
+    ax[0].set_ylabel('time [s]')
+    ax[1].set_ylabel('time [s]')
+
+    plt.xlabel('frame number')
+    plt.legend()
+    plt.savefig('timings/cpu_gpu_timed.png')
+
+
+
+    """
+    cap = cv2.VideoCapture(video_path)
+    frame_count = 0
+
+    while True:
+        success, frame = cap.read()
+        respons
+    """
+
